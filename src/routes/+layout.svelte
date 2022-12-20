f2c4757c136e34d7dc6f362d02c3e6eb4fc936e5
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2022-12-20 18:23:17 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,6 @@
 	import '../app.css';
 	import Header from "../components/Header.svelte"
 	import Footer from "../components/Footer.svelte"
-	import First from "../components/first.svelte"
-	import Second from "../components/second.svelte"
-	import Third from "../components/third.svelte"
-	import Fourth from "../components/fourth.svelte"
-	import Fifth from "../components/fifth.svelte"
-	import Sixth from "../components/sixth.svelte"
-	
 </script>
 	
 
