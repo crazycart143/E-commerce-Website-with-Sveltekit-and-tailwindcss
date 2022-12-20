5264a039e237da2d109d9b1d10dc53a823d0a7cf
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2022-12-20 18:38:31 +0800

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+	import Header from "../components/Header.svelte"
+	import Footer from "../components/Footer.svelte"
+</script>
+	
+
+
+<Header></Header>
+
+<main>
+<slot></slot>
+</main>
+
+<Footer></Footer>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+    <title>About Svelte Pokedex</title>
+</svelte:head>
+<h1>about page</h1>
