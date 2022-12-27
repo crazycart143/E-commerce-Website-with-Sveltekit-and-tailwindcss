77c2fa61c90b2d465076ee1a03792ffca8ca9ae3
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2022-12-27 15:24:57 +0800

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '../app.css';
+	import Header from "../components/Header.svelte"
+	import Footer from "../components/Footer.svelte"
+	import MobileMenu from '../components/MobileMenu.svelte';
+	
+	
+</script>
+	
+
+
+<Header></Header>
+
+<MobileMenu></MobileMenu>
+<main>
+<slot></slot>
+</main>
+
+<Footer></Footer>
