a9b622e2ef8f4c7b2e3a0449d371cdcfac0cbace
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2022-12-27 20:17:42 +0800

    Add files via upload

---
+++ src/components/MobileMenu.svelte
@@ -0,0 +1,14 @@
+<div class="flex fixed w-full z-50 lg:hidden pointer-events-none" id="mobileNavbar">
+<div class="drawer drawer-mobile " >
+    <input id="mobileNav" type="checkbox" class="drawer-toggle " />
+    <div class="drawer-side text-black">
+      <label for="mobileNav" class="drawer-overlay "></label> 
+      <div class="w-80 h-full p-4 bg-white ">
+        <li><a>Sidebar Item </a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </div>
+     
+    </div>
+  </div>
+</div>
+  
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import '../app.css';
+	import Header from "../components/Header.svelte"
+	import Footer from "../components/Footer.svelte"
+	import MobileMenu from '../components/MobileMenu.svelte';
+	
+	
+</script>
+	
+
+
+<Header></Header>
+
+
+<main>
+	<MobileMenu></MobileMenu>
+<slot></slot>
+</main>
+
+<Footer></Footer>
